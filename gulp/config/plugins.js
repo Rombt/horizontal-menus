4b0ee0d84d6fd5771545a54ba23204bd54b83b9c
roman <romron@ukr.net>
2023-02-23 19:14:31 +0200

    сборка gulp часть 2

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,10 +1,12 @@
-import replace from "gulp-replace";    // поиск и замена по регуляракам
-import plumber from "gulp-plumber";    // оброботка ошибок
-import notify from "gulp-notify";      // вывод соообщений об ошибках в windows!
+import replace from "gulp-replace"; // поиск и замена по регуляракам
+import plumber from "gulp-plumber"; // оброботка ошибок
+import notify from "gulp-notify"; // вывод соообщений об ошибках в windows!
+import browsersync from "browser-sync";
 
 
 export const plugins = {
-   replace: replace,
-   plumber: plumber,
-   notify: notify,
+    replace: replace,
+    plumber: plumber,
+    notify: notify,
+    browsersync: browsersync,
 }
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,27 +1,33 @@
 import gulp from "gulp";
-import { path } from "./gulp/config/path";
-import { reset } from "./gulp/tasks/reset";
-import { copy } from "./gulp/tasks/copy";
-import { php } from "./gulp/tasks/php";
-import { plugins } from "./gulp/config/plugins";
+import { reset } from "./gulp/tasks/reset.js";
+import { path } from "./gulp/config/path.js";
+import { copy } from "./gulp/tasks/copy.js";
+import { php } from "./gulp/tasks/php.js";
+import { plugins } from "./gulp/config/plugins.js";
+import { server } from "./gulp/tasks/server.js";
 
 global.app = {
-   path: path,
-   gulp: gulp,
-   plugins: plugins,       // оиск и замена по регуляракам 
+    path: path,
+    gulp: gulp,
+    plugins: plugins, // оиск и замена по регуляракам 
 }
+
+
+// let proxyPath = '';
+// if (__dirname.includes('OSPanel')) {
+//     proxyPath = "http://twily/gulp/build/"; // домашний комп
+// } else {
+//     proxyPath = "http://web/twily/gulp/build/"; // рабочий комп
+// }
 
 
 function watcher() {
-   gulp.watch(path.watch.files, copy)
-   gulp.watch(path.watch.php, php)
+    gulp.watch(path.watch.files, copy)
+    gulp.watch(path.watch.php, php)
 }
 
 const mainTasks = gulp.parallel(copy, php);
-const dev = gulp.series(reset, mainTasks, watcher);
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 
 
 gulp.task('default', dev);
-
-
-
