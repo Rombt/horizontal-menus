e5c3fd0ef6536908b36237b44efb583297b62bd6
Rombt <romron@ukr.net>
2024-01-20 14:24:19 +0200

    improved tasks/server.js

--- app/gulp/tasks/copyPHP.js
+++ app/gulp/tasks/copyPHP.js
@@ -8,19 +8,24 @@
 
 export const copyPHP = (done) => {
 
-   let srcPath;
+    let srcPath;
 
-   if (!app.path.copy && app.path.copy.src === undefined) {
-      return done();
-   } else if (Array.isArray(app.path.copy.src)) {
-      srcPath = app.path.copy.src.filter(el => !el.includes(app.path.srcPluginName));
-   } else if (typeof app.path.copy.src === 'string') {
-      srcPath = !app.path.copy.src.includes(app.path.srcPluginName) ? app.path.copy.src : '';
-   }
+    if (!app.path.copy || app.path.copy.src.length == 0) {
+        return done();
+    } else if (Array.isArray(app.path.copy.src)) {
+        srcPath = app.path.copy.src.filter(el => !el.includes(app.path.srcPluginName));
+    } else if (typeof app.path.copy.src === 'string') {
+        srcPath = !app.path.copy.src.includes(app.path.srcPluginName) ? app.path.copy.src : '';
+    }
 
-   return app.gulp.src(srcPath, {
-      allowEmpty: true,
-      base: app.path.src.php,
-   })
-      .pipe(app.gulp.dest((file) => app.path.selectDestPath(file, app.path.copy.dest)))
+
+    if (srcPath.length == 0) {
+        return done();
+    }
+
+    return app.gulp.src(srcPath, {
+            allowEmpty: true,
+            base: app.path.src.php,
+        })
+        .pipe(app.gulp.dest((file) => app.path.selectDestPath(file, app.path.copy.dest)))
 }
--- app/gulp/tasks/copyPl.js
+++ app/gulp/tasks/copyPl.js
@@ -8,18 +8,22 @@
 
 export const copyPl = (done) => {
 
-   let srcPath;
+    let srcPath;
 
-   if (!app.path.copy && app.path.copy.src === undefined) {
-      return done();
-   } else if (Array.isArray(app.path.copy.src)) {
-      srcPath = app.path.copy.src.filter(el => el.includes(app.path.srcPluginName));
-   } else if (typeof app.path.copy.src === 'string') {
-      srcPath = app.path.copy.src.includes(app.path.srcPluginName) ? app.path.copy.src : '';
-   }
+    if (!app.path.copy || app.path.copy.src.length == 0) {
+        return done();
+    } else if (Array.isArray(app.path.copy.src)) {
+        srcPath = app.path.copy.src.filter(el => el.includes(app.path.srcPluginName));
+    } else if (typeof app.path.copy.src === 'string') {
+        srcPath = app.path.copy.src.includes(app.path.srcPluginName) ? app.path.copy.src : '';
+    }
 
-   return app.gulp.src(srcPath, {
-      allowEmpty: true,
-   })
-      .pipe(app.gulp.dest((file) => app.path.selectDestPath(file, app.path.copy.dest)))
+    if (srcPath.length == 0) {
+        return done();
+    }
+
+    return app.gulp.src(srcPath, {
+            allowEmpty: true,
+        })
+        .pipe(app.gulp.dest((file) => app.path.selectDestPath(file, app.path.copy.dest)))
 }
