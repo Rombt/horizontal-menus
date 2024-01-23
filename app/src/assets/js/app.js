11aace4c38ada5d28ab1f79adfad27ee00660fb9
Rombt <romron@ukr.net>
2024-01-23 21:13:29 +0200

    что то

--- app/src/assets/js/app.js
+++ app/src/assets/js/app.js
@@ -1,4 +1,5 @@
 import "./modules/isWebp.js";
+import "./modules/mobile.js";
 import "./modules/dynamic_adapt.js";
 import "./modules/menu_burger.js";
-import "./modules/mobile.js";
+import "./modules/popup.js";
