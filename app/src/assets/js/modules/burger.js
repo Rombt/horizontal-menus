917ca25fb308e6e0ab86c0a915ad2ebc66e82084
Rombt <romron@ukr.net>
2024-01-30 21:20:17 +0200

    что то

--- app/src/assets/js/modules/burger.js
+++ app/src/assets/js/modules/burger.js
@@ -8,3 +8,29 @@ if (document.querySelector("html").classList.contains('touch')) {
         })
     }
 }
+
+
+const iconMenus = document.querySelectorAll('[data-iconMenu]');
+if (iconMenus.length > 0) {
+    iconMenus.forEach(iconMenu => {
+        iconMenu.addEventListener('click', (e) => {
+            const menu = iconMenu.closest('[data-conteinerMenu]');
+            const bodyMenu = menu.querySelector('[data-bodyMenu]');
+
+            if (bodyMenu.classList.contains('_openMenu')) {
+
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                bodyMenu.classList.remove('_openMenu');
+            } else {
+                document.body.classList.add('_lock');
+                iconMenu.classList.add('_active');
+                bodyMenu.classList.add('_openMenu');
+
+            }
+
+        })
+
+    });
+
+}
