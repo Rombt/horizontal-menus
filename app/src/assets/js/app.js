0a1fbd7c0dcb94f0b17935ee1e5d41b1673aa7cf
Rombt <Rombt@gmail.com>
2024-03-28 10:08:36 +0200

    minor corrections

--- app/src/assets/js/app.js
+++ app/src/assets/js/app.js
@@ -4,7 +4,7 @@ import "./modules/popup.js";
 
 import "./modules/burger.js";
 import "./modules/spoiler.js";
-import "./modules/tabs.js";
+// import "./modules/tabs.js";   // the file lost
 import "./modules/arrowsInputNumberStyle.js";
 
 
