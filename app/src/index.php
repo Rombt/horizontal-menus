2fb1d19a1c7d76e00e6f0008b6cf3cb32f7ed414
Rombt <Rombt@gmail.com>
2024-04-01 11:19:16 +0300

    important changes!

--- app/src/index.php
+++ app/src/index.php
@@ -2,180 +2,178 @@
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="./assets/styles/main-style.min.css" rel="stylesheet" type="text/css">
-	<title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./assets/styles/main-style.min.css" rel="stylesheet" type="text/css">
+    <title>Document</title>
 </head>
 
 <body>
 
-<div class="main-wrapper">
+    <div class="main-wrapper">
 
 
-<div class="container">
-	<div class="row">
-    <div class="cont-horisont-menu cont-red">
-        <div class="toggle-burge"><span></span></div>
-        <nav>
-            <ul>
-                <li><a href="#">item 1</a>
-                    <ul>
-                        <li><a href="#" >level 2 item 1</a>
-                            <ul>
-                                <li><a href="#">level 3 item 1</a></li>
-                                <li><a href="#">level 3 item 2</a></li>
-                                <li><a href="#">level 3 item 3</a></li>
-                                <li><a href="#">level 3 item 4</a></li>
-                                <li><a href="#">level 3 item 5</a></li>
-                                <li><a href="#">level 3 item 6</a></li>
-                                <li><a href="#">level 3 item 7</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#" >level 2 item 2</a>
-                        <li><a href="#" >level 2 item 3</a>
-                    </ul>
+        <div class="container">
+            <div class="row">
+                <div class="cont-horizont-menu cont-red">
+                    <div class="toggle-burge"><span></span></div>
+                    <nav>
+                        <ul>
+                            <li><a href="#">item 1</a>
+                                <ul>
+                                    <li><a href="#">level 2 item 1</a>
+                                        <ul>
+                                            <li><a href="#">level 3 item 1</a></li>
+                                            <li><a href="#">level 3 item 2</a></li>
+                                            <li><a href="#">level 3 item 3</a></li>
+                                            <li><a href="#">level 3 item 4</a></li>
+                                            <li><a href="#">level 3 item 5</a></li>
+                                            <li><a href="#">level 3 item 6</a></li>
+                                            <li><a href="#">level 3 item 7</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">level 2 item 2</a>
+                                    <li><a href="#">level 2 item 3</a>
+                                </ul>
 
-                </li>
-                <li><a href="#">item 2</a></li>
-                <li><a href="#">item 3</a></li>
-                <li><a href="#">item 4</a></li>
-                <li><a href="#">item 5</a></li>
-                <li><a href="#">item 6</a></li>
-                <li><a href="#">item 7</a></li>
-                <li><a href="#">item 8</a></li>
-                <li><a href="#">item 9999</a>
-                    <ul>
-                        <li><a href="#" ></a>
-                            <ul>
-                                <li><a href="#">level 2 item 1</a></li>
-                                <li><a href="#">level 2 item 2</a></li>
-                                <li><a href="#">level 2 item 3</a></li>
-                                <li><a href="#">level 2 item 4</a></li>
-                                <li><a href="#">level 2 item 5</a></li>
-                                <li><a href="#">level 2 item 6</a></li>
-                                <li><a href="#">level 2 item 7</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        	<div class="toggle-overflow-menu hidden"><span></span></div>
-        </nav>
+                            </li>
+                            <li><a href="#">item 2</a></li>
+                            <li><a href="#">item 3</a></li>
+                            <li><a href="#">item 4</a></li>
+                            <li><a href="#">item 5</a></li>
+                            <li><a href="#">item 6</a></li>
+                            <li><a href="#">item 7</a></li>
+                            <li><a href="#">item 8</a></li>
+                            <li><a href="#">item 9999</a>
+                                <ul>
+                                    <li><a href="#"></a>
+                                        <ul>
+                                            <li><a href="#">level 2 item 1</a></li>
+                                            <li><a href="#">level 2 item 2</a></li>
+                                            <li><a href="#">level 2 item 3</a></li>
+                                            <li><a href="#">level 2 item 4</a></li>
+                                            <li><a href="#">level 2 item 5</a></li>
+                                            <li><a href="#">level 2 item 6</a></li>
+                                            <li><a href="#">level 2 item 7</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="wrap-burger-menu cont-blue">
+                    <div class="toggle-burge"><span></span></div>
+                    <nav>
+                        <ul>
+                            <li><a href="#">item 1</a></li>
+                            <li><a href="#">item 2</a></li>
+                            <li><a href="#">item 3</a></li>
+                            <li><a href="#">item 4</a></li>
+                            <li><a href="#">item 5</a></li>
+                            <li><a href="#">item 6</a></li>
+                            <li><a href="#">item 7</a></li>
+                            <li><a href="#">item 8</a></li>
+                            <li><a href="#">item 9999</a>
+                                <ul>
+                                    <li><a href="#"></a>
+                                        <ul>
+                                            <li><a href="#">level 2 item 1</a></li>
+                                            <li><a href="#">level 2 item 2</a></li>
+                                            <li><a href="#">level 2 item 3</a></li>
+                                            <li><a href="#">level 2 item 4</a></li>
+                                            <li><a href="#">level 2 item 5</a></li>
+                                            <li><a href="#">level 2 item 6</a></li>
+                                            <li><a href="#">level 2 item 7</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="wrap-burger-menu cont-green">
+                    <div class="toggle-burge-menu"><span></span></div>
+                    <nav>
+                        <ul>
+                            <li><a href="#">item 1</a></li>
+                            <li><a href="#">item 2</a></li>
+                            <li><a href="#">item 3</a></li>
+                            <li><a href="#">item 4</a></li>
+                            <li><a href="#">item 5</a></li>
+                            <li><a href="#">item 6</a></li>
+                            <li><a href="#">item 7</a></li>
+                            <li><a href="#">item 8</a></li>
+                            <li><a href="#">item 9999</a>
+                                <ul>
+                                    <li><a href="#"></a>
+                                        <ul>
+                                            <li><a href="#">level 2 item 1</a></li>
+                                            <li><a href="#">level 2 item 2</a></li>
+                                            <li><a href="#">level 2 item 3</a></li>
+                                            <li><a href="#">level 2 item 4</a></li>
+                                            <li><a href="#">level 2 item 5</a></li>
+                                            <li><a href="#">level 2 item 6</a></li>
+                                            <li><a href="#">level 2 item 7</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <div class="show-hide-menu hidden"><span></span></div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div id="my-menu" class="cont-gray">
+                    <div class="toggle-burge-menu"><span></span></div>
+                    <nav>
+                        <ul>
+                            <li><a href="#">item 1</a></li>
+                            <li><a href="#">item 2</a></li>
+                            <li><a href="#">item 3</a>
+                                <ul>
+                                    <li><a href="#"></a>
+                                        <ul>
+                                            <li><a href="#">level 2 item 1</a></li>
+                                            <li><a href="#">level 2 item 2</a></li>
+                                            <li><a href="#">level 2 item 3</a></li>
+                                            <li><a href="#">level 2 item 4</a></li>
+                                            <li><a href="#">level 2 item 5</a></li>
+                                            <li><a href="#">level 2 item 6</a></li>
+                                            <li><a href="#">level 2 item 7</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <div class="show-hide-menu hidden"><span></span></div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+
     </div>
-	</div>
-</div>
-<div class="container">
-	<div class="row">
-    <div class="wrap-burger-menu cont-blue">
-        <div class="toggle-burge"><span></span></div>
-        <nav>
-            <ul>
-                <li><a href="#">item 1</a></li>
-                <li><a href="#">item 2</a></li>
-                <li><a href="#">item 3</a></li>
-                <li><a href="#">item 4</a></li>
-                <li><a href="#">item 5</a></li>
-                <li><a href="#">item 6</a></li>
-                <li><a href="#">item 7</a></li>
-                <li><a href="#">item 8</a></li>
-                <li><a href="#">item 9999</a>
-                    <ul>
-                        <li><a href="#" ></a>
-                            <ul>
-                                <li><a href="#">level 2 item 1</a></li>
-                                <li><a href="#">level 2 item 2</a></li>
-                                <li><a href="#">level 2 item 3</a></li>
-                                <li><a href="#">level 2 item 4</a></li>
-                                <li><a href="#">level 2 item 5</a></li>
-                                <li><a href="#">level 2 item 6</a></li>
-                                <li><a href="#">level 2 item 7</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        	<div class="toggle-overflow-menu hidden"><span></span></div>
-        </nav>
-    </div>
-	</div>
-</div>
-<div class="container">
-	<div class="row">
-    <div class="wrap-burger-menu cont-green">
-        <div class="toggle-burge-menu"><span></span></div>
-        <nav>
-            <ul>
-                <li><a href="#">item 1</a></li>
-                <li><a href="#">item 2</a></li>
-                <li><a href="#">item 3</a></li>
-                <li><a href="#">item 4</a></li>
-                <li><a href="#">item 5</a></li>
-                <li><a href="#">item 6</a></li>
-                <li><a href="#">item 7</a></li>
-                <li><a href="#">item 8</a></li>
-                <li><a href="#">item 9999</a>
-                    <ul>
-                        <li><a href="#" ></a>
-                            <ul>
-                                <li><a href="#">level 2 item 1</a></li>
-                                <li><a href="#">level 2 item 2</a></li>
-                                <li><a href="#">level 2 item 3</a></li>
-                                <li><a href="#">level 2 item 4</a></li>
-                                <li><a href="#">level 2 item 5</a></li>
-                                <li><a href="#">level 2 item 6</a></li>
-                                <li><a href="#">level 2 item 7</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        	<div class="show-hide-menu hidden"><span></span></div>
-        </nav>
-    </div>
-	</div>
-</div>
-<div class="container">
-	<div class="row">
-    <div id="my-menu" class="cont-gray">
-        <div class="toggle-burge-menu"><span></span></div>
-        <nav>
-            <ul>
-                <li><a href="#">item 1</a></li>
-                <li><a href="#">item 2</a></li>
-                <li><a href="#">item 3</a>
-                    <ul>
-                        <li><a href="#" ></a>
-                            <ul>
-                                <li><a href="#">level 2 item 1</a></li>
-                                <li><a href="#">level 2 item 2</a></li>
-                                <li><a href="#">level 2 item 3</a></li>
-                                <li><a href="#">level 2 item 4</a></li>
-                                <li><a href="#">level 2 item 5</a></li>
-                                <li><a href="#">level 2 item 6</a></li>
-                                <li><a href="#">level 2 item 7</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        	<div class="show-hide-menu hidden"><span></span></div>
-        </nav>
-    </div>
-	</div>
-</div>
-
-
-</div>
 
 
 
 
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="assets/js/libs/gsap.min.js"></script>
-<script src="assets/js/app.main.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="assets/js/libs/gsap.min.js"></script>
+    <script src="assets/js/app.main.min.js"></script>
 
 </body>
 
